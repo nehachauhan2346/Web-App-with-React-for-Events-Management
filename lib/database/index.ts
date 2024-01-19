beb1a8d8c1510732ce39153fda4ddcd481c60fc3
Neha Chauhan <c.neha15@gmail.com>
2024-01-20 03:52:17 +0530

    first commit

---
+++ lib/database/index.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGO_URI) throw new Error("Mongo URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_URI, {
+      dbName: "evently_db",
+      bufferCommands: false,
+    });
+  
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+
+    
+};
